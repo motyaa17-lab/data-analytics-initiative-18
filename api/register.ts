47ac772a2e0b83a7eb9691d6b985bddbb0a6d2f8
motyaa17-lab <motyaa17@icloud.com>
2026-02-28 19:40:55 +0500

    Update register.ts

--- api/register.ts
+++ api/register.ts
@@ -6,7 +6,7 @@ export default async function handler(req: any, res: any) {
   }
 
   const supabase = createClient(
-    process.env.VITE_SUPABASE_URL!,
+    process.env.SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   )
 
